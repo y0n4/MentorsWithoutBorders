6f2a50949195104dbcd2df3311cd0656e79d9720
Kavindu Amarasinghe <33703158+Kamarasinghe@users.noreply.github.com>
2018-06-23 10:32:22 -0700

    Added .jsx in line 5

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/index';
-import App from './components/App';
+import App from './components/App.jsx';
 
 render(
   <Provider store={store}>
