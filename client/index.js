9bf266c6122bb4eecc93cc1780a44dc280b492a8
Matthew Choi <mattjchoi@gmail.com>
2018-06-23 09:55:09 -0700

    Setup react, express, webpack

--- client/index.js
+++ client/index.js
@@ -1,10 +1,3 @@
-import React from 'react';
-<<<<<<< HEAD
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-ReactDOM.render( < App / > , document.getElementById('app'));
-=======
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/index';
@@ -16,4 +9,3 @@ render(
   </Provider>,
   document.getElementById('app')
 );
->>>>>>> Added App.jsx and main index.js
