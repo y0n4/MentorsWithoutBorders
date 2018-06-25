1fb8f25121a04fef8d43d4e5e926bb748240426c
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-06-25 11:50:56 -0700

    Created chat component and connected with App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Chat from './chat.jsx';
 
 const App = () => (
   <div className='main'>Mentors
     <button className='theButton googleOAuth'>
     </button>
     <div className='theButton'>
+      < Chat />
     </div>
   </div>
 );
