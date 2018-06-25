d51f1e524a01587bb6840b2643a9e73504535314
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-06-25 10:34:18 -0700

    Re-setup Jest-Enzyme test environment

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,6 @@
 import React from 'react';
 // Functions needed for testing
 import { configure, shallow, mount, render } from 'enzyme';
-// Enzyme adapter to use with React16
-import Adapter from 'enzyme-adapter-react-16';
-// Configuring a new adapter
-configure({ adapter: new Adapter() });
-
 
 import App from '../client/components/App';
 
@@ -17,6 +12,6 @@ describe('Component: App', () => {
   });
 
   it('should have a button for Google OAuth login', () => {
-    expect(wrapper.find('.theButton')).to.have.length(1);
+    expect(wrapper.hasClass('theButton')).toBe(true);
   });
 });
---
+++ tests/setupEnzyme.js
@@ -0,0 +1,5 @@
+import Enzyme from 'enzyme';
+// Enzyme adapter to use with React16
+import Adapter from 'enzyme-adapter-react-16';
+// Configuring a new adapter
+Enzyme.configure({ adapter: new Adapter() });
