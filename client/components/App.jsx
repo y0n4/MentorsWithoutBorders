0640e720809a83f5f60edc86a5022814d8ce1e0a
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-06-25 16:55:59 -0700

    Created a chat area

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import Chat from './chat.jsx';
+import openSocket from 'socket.io-client';
+import styles from '../css/app.css';
+const socket = openSocket('http://localhost:3000');
 
-const App = () => (
-  <div className='main'>Mentors
-    <button className='theButton googleOAuth'>
-    </button>
-    <div className='theButton'>
-      < Chat />
-    </div>
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      messages: []
+    }
+    
+    socket.on('test', (data) => {
+      this.setState({
+        messages: data
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className='main'>Mentors
+        <button className='theButton googleOAuth'>
+        </button>
+        <div className='theButton'>
+          < Chat messages={this.state.messages}/>
+        </div>
+      </div>
+    )
+  }
+
+}
 
 export default App;
