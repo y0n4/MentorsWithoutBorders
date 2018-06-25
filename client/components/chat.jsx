21f2554709fea9c82d5fb1c71e40c5026b64ae4f
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-06-25 15:23:58 -0700

    Successfully connected to socket

--- client/components/chat.jsx
+++ client/components/chat.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:3000');
 
 class chat extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      chat: []
+    }
+  }
 
   render() {
     return (
--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
 const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
 const port = process.env.PORT || 3000;
 const data = require('../database');
+
+io.on('connection', socket => {
+  console.log('New client connected')
+  socket.emit('test', { hello: 'world'});
+});
+
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.listen(port, function () {
+server.listen(port, function () {
   console.log(`Listening on port: ${port}`)
-})
+});
+
