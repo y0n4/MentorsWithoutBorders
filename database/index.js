fafda71c022343241c883ac8eeb198c27e457018
yona <04l.yona@gmail.com>
2018-06-25 14:52:23 -0400

    connect mongoose, address if connected or not

--- database/index.js
+++ database/index.js
@@ -2,3 +2,7 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/global-mentors');
 //mongodb uri is located in config.js file, it's connected to mlab, do l8r cuz only takes 500mb of data
+
+var db = mongoose.connection;
+db.on('error', () => console.log('AWWWW, DATABASE IS NOT CONNECTED :('));
+db.once('open', () => console.log('🌸 Database connected🌸 '));
