e8326fa475d30c89eb45b6a4c82b37398672e914
Matthew Choi <mattjchoi@gmail.com>
2018-06-26 11:10:33 -0700

    Create VideoComponent

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleMapUrl: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyChrCZCz5626kKD-53ilj-mYfbesnVwdvo&v=3.exp&libraries=geometry,drawing,places',
+  MONGO_URI: 'mongodb://mysk:hrr31mysk@ds217671.mlab.com:17671/globalmentors',
+  GOOGLE_ID: '390790142733-k08dau2l5pjrsgopke88olt0r9tmhc6c.apps.googleusercontent.com',
+  GOOGLE_SECRET: 'cyZDXXoCc6Scu0-kNXhXibtf',
+  TWILIO_ACCOUNT_SID: 'ACd2bbc4114282568bc3fd22275dfe1501',
+  TWILIO_API_KEY: 'SK921c5320d4c7b70dfed763037afb1fac',
+  TWILIO_API_SECRET: 'PyVCMwAk1tXvoQgjw41LoDRn9bAeBykI'
+};
