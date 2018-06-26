2bf866c9c0712fd7e61d04d434639aad2bb75658
selenax <selenawenxu@gmail.com>
2018-06-26 14:24:36 -0700

    gitignore

--- client/index.js
+++ client/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store/index';
-import App from './components/App.jsx';
-
-import AppBar from 'material-ui/AppBar';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store/index";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from "react-tap-event-plugin";
 injectTapEventPlugin();
+import App from "./components/App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MentorHome from "./components/MentorHome.jsx";
+import MenteeHome from "./components/MenteeHome.jsx";
+// import App from "./components/App.jsx";
 
-render(
-  <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-    <Provider store={store}>
-      <App />
-    </Provider>
+ReactDOM.render(
+<MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/mentor" component={MentorHome} />
+      <Route path="/mentee" component={MenteeHome} />
+    </div>
+  </Router>,
   </MuiThemeProvider>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
-
