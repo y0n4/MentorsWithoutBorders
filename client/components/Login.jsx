785ec2392db4b8d564aaa1515bec1b96d71eadcd
yona <04l.yona@gmail.com>
2018-06-27 01:30:07 -0400

    create sign in button for google oauth

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,7 +1,40 @@
 import React, { Component } from "react";
+// import $ from 'jquery'; //for request
+import axios from 'axios';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
 
+    // };
+  }
+
+  userLogin() {
+    // $.ajax({
+    //   url: '/auth/google',
+    //   type: 'GET',
+    //   contentType: 'application/json',      success: (loc) => {
+    //     console.log('worked');
+    //   },
+    //   error: (err) => {
+    //     console.error(err);
+    //   },
+    // });
+    
+    // axios.get('/auth/google')//endpoint
+    //   .then(res => {
+    //     console.log('worked!!');
+    //   })
+    //   .catch(err => {
+    //     console.log('too bad :(');
+    //   });
+  }
+
+  render() {
+    return <a href="/auth/google"><button className="login-btn">Sign in</button></a>
+  }
 }
 
 export default Login;
+
