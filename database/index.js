928c79f128de0061b1d180b204f3eff10742e765
yona <04l.yona@gmail.com>
2018-06-27 01:30:36 -0400

    extract proper information of logged in user

--- database/index.js
+++ database/index.js
@@ -3,12 +3,38 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/global-mentors');
 //mongodb uri is located in config.js file, it's connected to mlab, do l8r cuz only takes 500mb of data
 
+//importing all schemas
+var schemas = require('./schemas.js');
+
 var db = mongoose.connection;
 db.on('error', () => console.log('AWWWW, DATABASE IS NOT CONNECTED :('));
 db.once('open', () => console.log('🌸 Database connected🌸 '));
 
-//importing all schemas
-var schemas = require('./schemas.js');
 
 //⭐️ write funcs here⭐️
-//todo >> write func that saves user to db after google oauth
+
+//confirms if user exists in database
+var confirmUser = (googleId, callback) => {
+  schemas.User.find({'googleId': googleId}, (err, results) => {
+    if(err) callback(err, null);
+    else callback(null, results);
+  });
+};
+
+//saves user to database
+var saveUser = (googleId, fullName, callback) => {
+  var data = {
+    'googleId': googleId,
+    'fullName': fullName
+  }
+  var newUser = new schemas.User(data);
+  newUser.save((err, savedAccount) => {
+    if(err) callback(err, null);
+    else callback(null, savedEntry);
+  });
+};
+
+module.exports = {
+  confirmUser = confirmUser,
+  saveUser = saveUser
+}
