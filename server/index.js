1da1274899fe9cb8aefbc14068457381eb70eba4
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-06-26 17:30:38 -0700

    Rebased

--- server/index.js
+++ server/index.js
@@ -15,43 +15,6 @@ const app = express();
 const server = http.Server(app);
 const io = socketIo(server);
 
-//------------video stuffs------------//=
-
-require("dotenv").config();
-var path = require("path");
-var faker = require("faker");
-var AccessToken = require("twilio").jwt.AccessToken;
-var VideoGrant = AccessToken.VideoGrant;
-
-app.get("/token", function (request, response) {
-  var identity = faker.name.findName();
-
-  // Create an access token which we will sign and return to the client,
-  // containing the grant we just created
-  var token = new AccessToken(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_API_KEY,
-    process.env.TWILIO_API_SECRET
-  );
-
-  // Assign the generated identity to the token
-  token.identity = identity;
-
-  const grant = new VideoGrant();
-  // Grant token access to the Video API features
-  token.addGrant(grant);
-
-  // Serialize the token to a JWT string and include it in a JSON response
-  response.send({
-    identity: identity,
-    token: token.toJwt()
-  });
-});
-
-//------------video stuffs end------------//=
-
-
-
 //⬇⬇⬇ for google oauth ⬇⬇⬇
 const passport = require('passport'),
   auth = require('./auth'),
