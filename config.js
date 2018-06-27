1ec6ea519d1eea5ff2d886c7a16b6a145408818e
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-06-26 17:49:23 -0700

    Removed config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleMapUrl: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyChrCZCz5626kKD-53ilj-mYfbesnVwdvo&v=3.exp&libraries=geometry,drawing,places',
-  MONGO_URI: 'mongodb://mysk:hrr31mysk@ds217671.mlab.com:17671/globalmentors',
-  GOOGLE_ID: '390790142733-k08dau2l5pjrsgopke88olt0r9tmhc6c.apps.googleusercontent.com',
-  GOOGLE_SECRET: 'cyZDXXoCc6Scu0-kNXhXibtf',
-  TWILIO_ACCOUNT_SID: 'ACd2bbc4114282568bc3fd22275dfe1501',
-  TWILIO_API_KEY: 'SK7e49fd99516924e11ce9a84482a26a00',
-  TWILIO_API_SECRET: 'UzRnFowV9vyqT86tsHeptEH5XJYcYGwC'
-};
