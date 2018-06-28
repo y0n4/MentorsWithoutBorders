1b62552f602d37663adaab2aa2e5705ae8135edc
Matthew Choi <mattjchoi@gmail.com>
2018-06-28 11:11:44 -0700

    Fix vectormap

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,13 +5,13 @@ import VectorMap from 'react-jvectormap';
 
 const Home = props => {
   return (
-    <div style={{ width: 500, height: 500 }}>
+    <div>
       <VectorMap map={'world_mill'}
         backgroundColor="#dfebf6"
         ref="map"
         containerStyle={{
-          width: '100%',
-          height: '100%'
+          width: '50%',
+          height: '50%'
         }}
         containerClassName="map"
       />
