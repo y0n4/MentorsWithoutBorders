e2270d0cd40db1d84a7d58ed9edd955a778bbc20
Kavindu Amarasinghe <33703158+Kamarasinghe@users.noreply.github.com>
2018-06-28 10:17:44 -0700

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -1,6 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const GOOGLE_ID = process.env.GOOGLE_ID || require('../config').GOOGLE_ID;
-const clientSecret = process.env.clientSecret || require('../config').clientSecret;
+const GOOGLE_SECRET = process.env.GOOGLE_SECRET || require('../config').GOOGLE_SECRET;
 const callbackURL = 'https://mentors-beyond-borders.herokuapp.com/auth/google/callback';
 
 module.exports = (passport) => {
