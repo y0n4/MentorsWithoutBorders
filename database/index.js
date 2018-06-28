139bcb8b8918601a0c2afd5a3532a231ee207a94
yona nagayama <04l.yona@gmail.com>
2018-06-28 14:10:37 -0400

    change mongo uri

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 //currently mongoose will refactor l8r to a diff database like postgresql
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/global-mentors');
+mongoose.connect(process.env.MONGO_URI);
 //mongodb uri is located in config.js file, it's connected to mlab, do l8r cuz only takes 500mb of data
 
 //importing all schemas
