22419c5a817d698a022c8b54bf412704313485b7
Matthew Choi <mattjchoi@gmail.com>
2018-06-28 11:28:19 -0700

    Revert map

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,26 +1,12 @@
 import React from "react";
 import Map from "./Map.jsx";
 // import { API } from "../../config.js";
-import VectorMap from 'react-jvectormap';
 
 const Home = props => {
   return (
     <div>
-      <VectorMap map={'world_mill'}
-        backgroundColor="#dfebf6"
-        ref="map"
-        containerStyle={{
-          width: '50%',
-          height: '50%'
-        }}
-        containerClassName="map"
-      />
-    </div>
-  );
-};
-
-export default Home;
-{/* <Map
+      what up
+      {/* <Map
         locations={[
           { lat: 38, lng: 90 },
           { lat: 60, lng: 70 },
@@ -31,3 +17,8 @@ export default Home;
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%`, width: `50%` }} />}
       /> */}
+    </div>
+  );
+};
+
+export default Home;
