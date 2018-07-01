f203e8dd02926820c68832cba1fa82aa715bebd5
yona <04l.yona@gmail.com>
2018-06-30 20:04:31 -0400

    refactor from mongoose to sequelize

--- database/index.js
+++ database/index.js
@@ -1,40 +1,61 @@
-// currently mongoose will refactor l8r to a diff database like postgresql
-const mongoose = require('mongoose');
+const Sequelize = require('sequelize');
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/global-mentors');
-// mongodb uri is located in config.js file, it's connected to mlab, do l8r cuz only takes 500mb of data
+const sequelize = new Sequelize('postgres://localhost:8080/globalmentors');
 
-// importing all schemas
-const schemas = require('./schemas.js');
-
-const db = mongoose.connection;
-db.on('error', err => console.log('AWWWW, DATABASE IS NOT CONNECTED :(', err));
-db.once('open', () => console.log('🌸 Database connected🌸 '));
-
-
-// ⭐️ write funcs here⭐️
-
-// confirms if user exists in database
-const findUser = (googleId, callback) => {
-  schemas.User.find({ googleId }, (err, results) => {
-    console.log(googleId, 19);
-    if (err) callback(err, null);
-    else callback(null, results);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('🔥 🔥 database connected🔥 🔥');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
   });
+
+// model represents a table in database
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  googleId: Sequelize.STRING,
+  fullName: Sequelize.STRING,
+  gender: Sequelize.STRING,
+  ratings: Sequelize.INTEGER,
+  totalRatings: Sequelize.INTEGER,
+  bio: Sequelize.STRING,
+  isMentor: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+  isMentee: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+  mentors: Sequelize.ARRAY(Sequelize.TEXT),
+  mentees: Sequelize.ARRAY(Sequelize.TEXT),
+  blocked: Sequelize.ARRAY(Sequelize.TEXT),
+  location: Sequelize.JSON,
+});
+// can also write getterMethods and setterMethods (http://docs.sequelizejs.com/manual/tutorial/models-definition.html#getters-setters)
+
+// sync model to database
+User.sync({ force: true }).then(() => {
+  // Table created
+  console.log('model is synced');
+}).catch((err) => {
+  console.log('model is not synced');
+});
+
+// confirm if user exists in database
+const findUser = (googleId, callback) => {
+
 };
 
 // saves user to database
 const saveUser = (query, callback) => {
-  const newUser = new schemas.User(query);
-  newUser.save((err, savedAccount) => {
-    if (err) {
-      console.log(err);
-      callback(err, null);
-    } else callback(null, savedAccount);
-  });
-};
-
-module.exports = {
-  findUser,
-  saveUser,
+  User.create({query})
+  .them
 };
