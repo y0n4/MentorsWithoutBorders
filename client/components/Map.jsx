294c4854f9bc09d498c7ac0fa1beac1bab8039d3
Matthew Choi <mattjchoi@gmail.com>
2018-07-05 11:20:10 -0700

    Adjust map props

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -6,53 +6,39 @@ class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      point: [],
       ready: '',
     }
     this.addData = this.addData.bind(this);
     this.renderMap = this.renderMap.bind(this);
   }
-
-  addData() {
-    // console.log(this.refs.map.getMapObject())
-    // console.log(this.refs.map.getMapObject().scale = ['#C8EEFF', '#0071A4']);
-  }
-
-  componentDidMount() {
-    axios.get('/map')
-      .then((res) => {
-        console.log('got it', res.data);
-        this.setState({point: res.data});
-        // console.log(this.state.userLocation);
-      })
       .catch((err) => {
-        console.error(err);
-      });
+  console.error(err);
+});
   }
 
-  renderMap(data) {
-    return (
-      <VectorMap
-        map="world_mill"
-        ref="map"
-        backgroundColor="clear"
-        containerStyle={{
-          width: '100%',
-          height: '100%',
-        }}
-        containerClassName="map"
-        markers={data}
-      />
-    );
-  }
+renderMap(data) {
+  return (
+    <VectorMap
+      map="world_mill"
+      ref="map"
+      backgroundColor="clear"
+      containerStyle={{
+        width: '100%',
+        height: '100%',
+      }}
+      containerClassName="map"
+      markers={data}
+    />
+  );
+}
 
-  render() {
-    return (
-      <div className="world-map">
+render() {
+  return (
+    <div className="world-map">
       {this.state.point.length < 1 ? <div>Nada</div> : this.renderMap(this.state.point)}
-      </div>
-    )
-  }
+    </div>
+  )
+}
 }
 
 export default Map;
