170014e5fc8de83818edae28bfe22d9658735225
Matthew Choi <mattjchoi@gmail.com>
2018-07-05 03:40:33 -0700

    Add vectormap to home comp

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,36 +1,15 @@
-import React from 'react';
-import Map from './Map';
-// import { API } from "../../config.js";
+import React, { Component } from 'react'
+import { VectorMap } from 'react-jvectormap';
+import Map from './Map'
 
-const Home = props => (
-  <div>
-    what up
-    <div style={{ width: 500, height: 500 }}>
-      <VectorMap
-map="us_aea"
-        backgroundColor="#3b96ce"
-        ref="map"
-        containerStyle={{
-          width: '100%',
-          height: '100%',
-        }}
-        containerClassName="map"
-      />
+class Home extends React.Component {
+  render() {
+    return (
+    <div className="world-map">
+      <Map />
     </div>
-
-
-    {/* <Map
-        locations={[
-          { lat: 38, lng: 90 },
-          { lat: 60, lng: 70 },
-          { lat: 20, lng: 40 }
-        ]} // pass in geolocations props
-        googleMapURL={API.googleMapUrl}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%`, width: `50%` }} />}
-      /> */}
-  </div>
-);
+    );
+  }
+}
 
 export default Home;
--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,46 +1,43 @@
 import React from 'react';
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from 'react-google-maps';
-import VectorMap from 'react-jvectormap';
+import { VectorMap } from 'react-jvectormap';
+import axios from 'axios';
 
 class Map extends React.Component {
-  changeBg() {
-    this.refs.map.setBackgroundColor('red');
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    this.addData = this.addData.bind(this);
+  }
+  componentDidMount() {
+    // this.addData();
+    // axios.get('/mentor-count')
+    //   .then(({data}) => {
+    //     this.refs.map.getMapObject().regionsData = data.mapData
+      // })
+  }
+
+  addData() {
+    // console.log(this.refs.map.getMapObject())
+    // console.log(this.refs.map.getMapObject().scale = ['#C8EEFF', '#0071A4']);
   }
 
   render() {
     return (
-      <div style={{ width: 500, height: 500 }}>
-        <VectorMap
-          map="world_mill"
-          backgroundColor="#3b96ce"
-          ref="map"
-          containerStyle={{
-            width: '100%',
-            height: '100%',
-          }}
-          containerClassName="map"
-        />
-      </div>
+      <VectorMap
+        map="world_mill"
+        className="world-map"
+        ref="map"
+        backgroundColor="clear"
+        containerStyle={{
+          width: '100%',
+          height: '100%',
+        }}
+        containerClassName="map"
+      />
     );
   }
 }
-// const Map = withScriptjs(withGoogleMap(props => (
-//   <GoogleMap
-//     defaultZoom={1}
-//     defaultCenter={{ lat: 39.828, lng: 98.579 }}
-//   >
-//     {props.locations.map((mentor, i) => (
-//       <Marker
-//         key={i}
-//         position={{ lat: mentor.lat, lng: mentor.lng }}
-//       />
-//     ))}
-//   </GoogleMap>
-// )));
 
 export default Map;
