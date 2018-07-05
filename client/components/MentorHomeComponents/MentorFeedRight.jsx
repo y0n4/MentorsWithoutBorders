55b5378a0ec33afb14affd2fa14d708e4f2c42af
yona <04l.yona@gmail.com>
2018-07-04 23:26:32 -0400

    Initialize component for online mentees for mentor homepage

---
+++ client/components/MentorHomeComponents/MentorFeedRight.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+function MentorFeedRight(props) {
+  return (
+    <div>
+    <div className="mentee-online">
+      <img src="http://i64.tinypic.com/20uwyl0.jpg" width="50" height="50" alt=""/><br />
+      <strong>Wade Watts</strong>
+    </div>
+    </div>
+  )
+}
+
+export default MentorFeedRight;
