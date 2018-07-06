c748ae22b4ef0107a0de0b10d88b8b5bdcb0586e
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-06 11:15:36 -0700

    Seperated out name and state into divs

--- client/components/MentorSearchComponents/SelectedMentors.jsx
+++ client/components/MentorSearchComponents/SelectedMentors.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import Avatar from '@material-ui/core/Avatar';
 
 export default class SelectedMentors extends Component {
   render() {
+    let mentor = this.props.mentor;
+
     return (
       <div className='eachMentor'>
-        <img src={this.props.mentor.picture.thumbnail} />
+        <ListItem>
+          <Avatar src={mentor.picture.thumbnail} className='theAvatar' />
+            <div className='theName'>
+              <h2>{`${mentor.name.first} ${mentor.name.last}`}</h2> 
+            </div>
+            <div className='state'>
+              <h3>{mentor.location.state}</h3> 
+            </div>
+        </ListItem>
+        {console.log(this.props.mentor)}
       </div>
     );
   }
