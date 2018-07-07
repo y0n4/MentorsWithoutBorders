d6b886277f1ee69f662fc59ab50de453ad2b7ed1
yona <04l.yona@gmail.com>
2018-07-07 01:38:52 -0400

    Add axios to get info of visitor's info

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import Map from './Map';
+import axios from 'axios';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    }
-  };
+    };
+  }
+
+  // func to get users location using api call
+  componentDidMount() {
+    console.log('oi');
+    // then save to db
+    axios({
+      method:'get',
+      url:'https://geoip-db.com/jsonp',
+      responseType:'json'
+    })
+    .then(function(response) {
+      console.log('geolocation', response);
+    });
+  }
 
   render() {
     return (
