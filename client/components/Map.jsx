b400e208d082497d64393ed6adc16f9478cc1a1a
yona <04l.yona@gmail.com>
2018-07-06 23:49:11 -0400

    get the map to render user points if there is length state, ternary operator

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -6,52 +6,11 @@ class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userLocation: 
-        [{latLng: [41.90, 12.45], name: 'Vatican City'},
-        {latLng: [43.73, 7.41], name: 'Monaco'},
-        {latLng: [-0.52, 166.93], name: 'Nauru'},
-        {latLng: [-8.51, 179.21], name: 'Tuvalu'},
-        {latLng: [43.93, 12.46], name: 'San Marino'},
-        {latLng: [47.14, 9.52], name: 'Liechtenstein'},
-        {latLng: [7.11, 171.06], name: 'Marshall Islands'},
-        {latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},
-        {latLng: [3.2, 73.22], name: 'Maldives'},
-        {latLng: [35.88, 14.5], name: 'Malta'},
-        {latLng: [12.05, -61.75], name: 'Grenada'},
-        {latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
-        {latLng: [13.16, -59.55], name: 'Barbados'},
-        {latLng: [17.11, -61.85], name: 'Antigua and Barbuda'},
-        {latLng: [-4.61, 55.45], name: 'Seychelles'},
-        {latLng: [7.35, 134.46], name: 'Palau'},
-        {latLng: [42.5, 1.51], name: 'Andorra'},
-        {latLng: [14.01, -60.98], name: 'Saint Lucia'},
-        {latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
-        {latLng: [1.3, 103.8], name: 'Singapore'},
-        {latLng: [1.46, 173.03], name: 'Kiribati'},
-        {latLng: [-21.13, -175.2], name: 'Tonga'},
-        {latLng: [15.3, -61.38], name: 'Dominica'},
-        {latLng: [-20.2, 57.5], name: 'Mauritius'},
-        {latLng: [26.02, 50.55], name: 'Bahrain'},
-        {latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'}]
+      point: [],
+      ready: '',
     }
     this.addData = this.addData.bind(this);
-  }
-  componentDidMount() {
-    // this.addData();
-    // axios.get('/mentor-count')
-    //   .then(({data}) => {
-    //     this.refs.map.getMapObject().regionsData = data.mapData
-      // })
-
-    console.log('oi')
-    axios.get('/map')
-      .then((res) => {
-        this.setState({userLocation: res.data});
-        console.log(this.state.userLocation);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    this.renderMap = this.renderMap.bind(this);
   }
 
   addData() {
@@ -59,11 +18,22 @@ class Map extends React.Component {
     // console.log(this.refs.map.getMapObject().scale = ['#C8EEFF', '#0071A4']);
   }
 
-  render() {
+  componentDidMount() {
+    axios.get('/map')
+      .then((res) => {
+        console.log('got it', res.data);
+        this.setState({point: res.data});
+        // console.log(this.state.userLocation);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  renderMap(data) {
     return (
       <VectorMap
         map="world_mill"
-        className="world-map"
         ref="map"
         backgroundColor="clear"
         containerStyle={{
@@ -71,9 +41,17 @@ class Map extends React.Component {
           height: '100%',
         }}
         containerClassName="map"
-        markers={this.state.userLocation}
+        markers={data}
       />
     );
+  }
+
+  render() {
+    return (
+      <div className="world-map">
+      {this.state.point.length < 1 ? <div>Nada</div> : this.renderMap(this.state.point)}
+      </div>
+    )
   }
 }
 
