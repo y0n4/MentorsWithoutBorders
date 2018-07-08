325ffd8c7c612e6c4c08065698009b733d5f6aab
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-07 17:21:35 -0700

    Pseudocoded recommendation functions for topic and age range

---
+++ database/Recommendations/recommenderFuncs.js
@@ -0,0 +1,28 @@
+/*
+  Mentors mentoring in interested topics {
+    - Passed all mentors as array
+    - Returns array of mentors that specialize in 
+      topics the current user is interested in
+  }
+
+  Mentors that are within a certain age {
+    - Takes in array passed from above function
+    - Takes in number to restrict age gap, defaults to 5
+    
+    - Filter array for mentors within same age as current user
+    - If resulting array's length is less than 10
+      - Invoke function again but add 5 to current restrict num
+    
+    - Return filtered array
+  }
+
+  Mentors active around same time {
+    - Takes in array passed from above function
+    
+    - Filter array for mentors that are active same time as user
+    - If resulting array is less than 10
+      - Rearrange 
+  }
+  
+  ** Probably should add a score key to mentor data
+*/
