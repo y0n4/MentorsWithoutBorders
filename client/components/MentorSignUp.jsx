1f827321a6821b6f8bfdf48eec74f6aba1edc885
yona <04l.yona@gmail.com>
2018-07-10 18:12:51 -0400

    Add new description for mentees to sign up as mentors

--- client/components/MentorSignUp.jsx
+++ client/components/MentorSignUp.jsx
@@ -4,17 +4,29 @@ import Modal from '@material-ui/core/Modal';
 class MentorSignUp extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      isMentor: this.props.isMentor
+    }
+    this.isMentorNow = this.isMentorNow.bind(this);
+  }
+
+  isMentorNow() {
+    console.log('clicked', this.state.isMentor);
+    this.setState({ isMentor: true });
+    // update to sequelize
   }
 
   render() {
     return ( 
+      <div>
+      <h2>So you want to be in our mentoring community?</h2>
       <div className="mentor-descrip">
-      <h2>So you want to be a mentor, eh?</h2>
-        Anyone can be a mentor in our community. Become a mentor and feel the external satisfaction of helping a mentee grow and hone their experience in your field! Not only are you giving, you may even feel like you are also learning along the way! <br />
+        Anyone can be a mentor in our community. It feels so rewarding when helping a fellow mentee grow, gain knowledge, and be thankful for your efforts! Not only are you giving, you are also learning to be patient, understanding, and more about yourself along the way. <br />
         <br />
 
-        Please fill out the form below.
+        Simply click on the button below!
+        <button className="login-btn" onClick={this.isMentorNow}>Sign me up!</button>
+      </div>
       </div>
     )
   }
