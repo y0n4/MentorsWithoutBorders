a0e4e0baf19206fcf8427ad6d3beb11f356a7793
selenax <selenawenxu@gmail.com>
2018-07-10 15:01:24 -0700

    added personality and twitter api for trait analysis

---
+++ server/personality.js
@@ -0,0 +1,48 @@
+
+require('dotenv').config({ path: '../.env' });
+const readline = require('readline');
+const PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+const PersonalityTextSummaries = require('personality-text-summary');
+const fetchTweets = require('./fetchTweets');
+
+const personality_insights = new PersonalityInsightsV3({
+  username: process.env.PERSONALITY_INSIGHTS_USERNAME,
+  password: process.env.PERSONALITY_INSIGHTS_PASSWORD,
+  version_date: '2017-12-01',
+});
+
+
+const v3EnglishTextSummaries = new PersonalityTextSummaries({
+  locale: 'en',
+  version: 'v3',
+});
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Analyze me meowwww...', (handle) => {
+  console.log('Your results should show up soon. Thank you.');
+
+  fetchTweets(handle).then((tweets) => {
+    const params = {
+      // Content items are tweets.
+      content_items: tweets,
+      consumption_preferences: true,
+      raw_scores: true,
+      headers: {
+        'accept-language': 'en',
+        accept: 'application/json',
+      },
+    };
+    personality_insights.profile(params, (error, personalityProfile) => {
+      if (error && error.code == 400) {
+        reject(Error('Ouch! You either do not have sufficient tweets, or your language is not supported. Sorry.'));
+      } else { console.log(JSON.stringify(personalityProfile, null, 2)); }
+    });
+
+    rl.close();
+  }).catch(err => console.error(err));
+});
