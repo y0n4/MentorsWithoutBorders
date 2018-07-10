68f9d0c85bb4c208a1315257564cc00f0cad6063
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-09 22:09:45 -0700

    Created functions for recommending based on similar grammar

--- database/Recommendations/wordCount.js
+++ database/Recommendations/wordCount.js
@@ -49,27 +49,6 @@
   }
 */
 
-/*
-  Getting a users word count {
-    - Track when a user logs in/logs out
-    - When user logs out run the functions
-
-    - Gather messages user has entered between login and logout
-    - Grab the current wordCount object from user database
-    - Split messages, delimited by spaces
-    - Iterate through words
-    - If current word is curse word 
-      - Increment 'curse' key's value by 1
-    - Else if current word is inside wordCount
-      - Increment the count by 1
-    - Else
-      - Add the word to wordCount setting it equal to 1
-    - Return wordCount object
-
-    - Store the wordCount object back into user database
-  }
-*/
-
 let userWordCounts = (wordCounts, messages) => {
   let words = messages.split(' ');
   let cussWords = [];
@@ -77,8 +56,8 @@ let userWordCounts = (wordCounts, messages) => {
   let userWordCount = words.reduce((wordCount, word) => {
     if (cussWords.indexOf(word) > -1) {
       wordCounts.cussCount++;
-    } else if (wordCount[word]) {
-      wordCount[word]++; 
+    } else if (wordCounts[word]) {
+      wordCounts[word]++; 
     } else {
     	wordCounts[word] = 1
     }
@@ -86,6 +65,62 @@ let userWordCounts = (wordCounts, messages) => {
     return wordCount;
   }, wordCounts);
 
-  return userWordCount;
+  user.wordCount = userWordCount;
 };
 
+/*
+  Providing recommendation based on wordCount {
+    - Takes in an array of users
+    - Takes in the current users's wordCount
+
+    - Turn current user's wordCount into an array of objects
+    - Sort array from most used to least
+    - Grab first 20 if length greater
+
+    - Create a new array recommendedMentors
+    - Iterate through each user
+      - Pass first20 array and currentUser's wordCount
+        to matchScore function
+      - Push result to recommendedMentors
+    
+    - Return recommendedMentors
+  }
+*/
+
+let getWordCountScore = (mentorWordCount, userWordCount) => {
+  let score = 0;
+
+  for (let word in userWordCount) {
+    let uCount = userWordCount[word];
+    let mCount = mentorWordCount[word] || null;
+
+    if (mCount) {
+      let diff = Math.abs(uCount - mCount);
+      
+      if (diff <= 10) {
+        score += 20;
+      } else if (diff > 10 && diff <= 20) {
+        score += 10;
+      } else if (diff > 20 && diff <= 30) {
+        score += 7; 
+      } else if (diff > 30 && diff <= 40) {
+        score += 5;
+      } else if (diff > 40 && diff <= 50) {
+        score += 2; 
+      }
+    }
+  }
+
+  return score;
+};
+
+let wordCountScore = (mentors) => {
+  let userWordCount = user.wordCount;
+
+  mentors.forEach((mentor) => {
+    let mentorWordCount = mentor.wordCount
+    let score = getWordCountScore(userWordCount, mentorWordCount);
+
+    mentor.score += score;
+  });
+};
