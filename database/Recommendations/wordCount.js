54dccafeed9dd6b3b006321fcaa779d78dd84e79
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-09 17:35:50 -0700

    Created function to track popular words that an individual user uses

--- database/Recommendations/wordCount.js
+++ database/Recommendations/wordCount.js
@@ -48,3 +48,44 @@
     - Return recommendedMentors
   }
 */
+
+/*
+  Getting a users word count {
+    - Track when a user logs in/logs out
+    - When user logs out run the functions
+
+    - Gather messages user has entered between login and logout
+    - Grab the current wordCount object from user database
+    - Split messages, delimited by spaces
+    - Iterate through words
+    - If current word is curse word 
+      - Increment 'curse' key's value by 1
+    - Else if current word is inside wordCount
+      - Increment the count by 1
+    - Else
+      - Add the word to wordCount setting it equal to 1
+    - Return wordCount object
+
+    - Store the wordCount object back into user database
+  }
+*/
+
+let userWordCounts = (wordCounts, messages) => {
+  let words = messages.split(' ');
+  let cussWords = [];
+
+  let userWordCount = words.reduce((wordCount, word) => {
+    if (cussWords.indexOf(word) > -1) {
+      wordCounts.cussCount++;
+    } else if (wordCount[word]) {
+      wordCount[word]++; 
+    } else {
+    	wordCounts[word] = 1
+    }
+
+    return wordCount;
+  }, wordCounts);
+
+  return userWordCount;
+};
+
