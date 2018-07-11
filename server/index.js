3c569a737407b91b250a2f1ee9b5c5480f9acb45
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-11 13:47:37 -0700

    Handling updating the wordCount of the user when logging out

--- server/index.js
+++ server/index.js
@@ -29,6 +29,7 @@ const auth = require('./auth');
 const exampleData = require('./exampleData').exampleMessages;
 const userData = require('../database/dummyGen/users').userList.results;
 const { getCategoryIds } = require('./extractingInfo');
+const { userWordCounts } = require('../database/Recommendations/wordCount');
 // temp stuff
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -51,12 +52,14 @@ const port = process.env.PORT || 3000;
 const data = require('../database');
 
 const users = {};
+let messages;
 
 io.on('connection', (socket) => {
   console.log('✅  Socket Connection from id:', socket.id);
   users[socket.id] = {};
   socket.emit('loginCheck');
   let logInTime = new Date().getHours();
+  messages = [];
 
   socket.on('userLoggedIn', (client) => {
     console.log('🔑🔑🔑 ', client.name, 'Logged In', client);
@@ -65,6 +68,8 @@ io.on('connection', (socket) => {
       name: client.name,
       photo: client.photo,
     };
+
+    users.userId = client.userId;
     console.log('✅✅✅getmy', users[socket.id].userId);
     data.loginUser(client.userId, socket.id);
     data.getMyMentors(users[socket.id].userId, (mentors) => {
@@ -83,6 +88,10 @@ io.on('connection', (socket) => {
 
   socket.on('new message', (message) => {
     console.log('✉️ socket.new message', message);
+    // Save message to message database
+    data.setMessage(users[socket.id].userId, message.message, 1);
+
+    messages.push(message.message);
     socket.broadcast.emit('new message', message);
     io.to(socket.id).emit('new message', message);
   });
@@ -107,12 +116,25 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     // console.log('⛔ ', users[socket.id], 'Disconnected from socket');
-    io.emit('userDisconnect', socket.id);
-    console.log(users[socket.id].userId);
-    data.logoutUser(users[socket.id].userId);
     let logOutTime = new Date().getHours();
-    data.setAvgLoggedInTime(users[socket.id].userId, logInTime, logOutTime);
-    delete users[socket.id];
+    // let userId = users[socket.id].userId;
+    data.setAvgLoggedInTime(users.userId, logInTime, logOutTime);
+    data.findUserById(users.userId, (user) => {
+
+      if (messages.length !== 0) {
+        let updatedUserWordCount = userWordCounts(user, messages);
+        
+        data.updateUserWordCount(users.userId, updatedUserWordCount, () => {
+          io.emit('userDisconnect', socket.id);
+          data.logoutUser(users[socket.id].userId);
+          delete users[socket.id];
+        });
+      } else {
+        io.emit('userDisconnect', socket.id);
+        data.logoutUser(users[socket.id].userId);
+        delete users[socket.id];
+      }
+    });
   });
 });
 
@@ -245,6 +267,26 @@ app.get('/recommendation', (req, res) => {
     });
   });
 });
+
+// app.get('/generateMessages', (req, res) => {
+
+//   axios({
+//     method: 'get',
+//     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies&count=10', 
+//     headers: {
+//     'X-Mashape-Key': 'czGDnXNx1gmshgfCx4vYASFY9Bnsp1ksXifjsnIGGtctpIGWtU'
+//     }
+//   }).then((results) => {
+//     results.data.forEach((quote) => {
+//       data.setMessage(2, quote.quote, 1);
+//     })
+    
+//     console.log('It ran')
+//     res.send('200')
+//   }).catch((err) => {
+//     console.log('Err from results', err);
+//   })
+// });
 
 app.get('/allMentors', (req, res) => {
   res.send(userData);
