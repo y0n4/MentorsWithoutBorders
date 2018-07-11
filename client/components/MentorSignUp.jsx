81dc73d6530568ae24280ecdc28f09bf833e50d4
yona <04l.yona@gmail.com>
2018-07-11 19:27:26 -0400

    Render description to sign up as mentor page and later redirects user to mentor mode

--- client/components/MentorSignUp.jsx
+++ client/components/MentorSignUp.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect, Link } from 'react-router-dom';
 import Modal from '@material-ui/core/Modal';
 
 class MentorSignUp extends Component {
@@ -22,17 +23,40 @@ class MentorSignUp extends Component {
       });
   }
 
+  checkMentor() {
+    if (!this.state.isMentor) {
+      return (
+        <div className="mentor-descrip">
+          <h2>So you want to be in our mentoring community?</h2>
+
+          Anyone can be a mentor in our community. It feels so rewarding when helping a fellow mentee grow, gain knowledge, and be thankful for your efforts! Not only are you giving, you are also learning to be patient, understanding, and more about yourself along the way. <br />
+          <br />
+  
+          Simply click on the button below!<br /><br />
+          <button className="login-btn" onClick={this.isMentorNow}>
+            Sign me up!
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="mentor-descrip">
+          <h2>Congrats you're a mentor!</h2>
+          Thanks for singing up! You now have access to Mentor Mode and see questions posted by mentees around the world! You can view these questions in all categories or filtering them out with specific ones instead. Just chat with them to answer their question!<br /><br />
+          <button className="login-btn">
+          <Link to="/mentor">
+            Let's go!
+          </Link>
+          </button>
+        </div>
+      );
+    }
+  }
+
   render() {
     return ( 
       <div>
-      <h2>So you want to be in our mentoring community?</h2>
-      <div className="mentor-descrip">
-        Anyone can be a mentor in our community. It feels so rewarding when helping a fellow mentee grow, gain knowledge, and be thankful for your efforts! Not only are you giving, you are also learning to be patient, understanding, and more about yourself along the way. <br />
-        <br />
-
-        Simply click on the button below!
-        <button className="login-btn" onClick={this.isMentorNow}>Sign me up!</button>
-      </div>
+        {this.checkMentor()}
       </div>
     )
   }
