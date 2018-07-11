88171dd4e136a7952f7327e30471f86789333685
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-11 00:03:47 -0700

    Passed in currentUser data into functions

--- database/Recommendations/recommendationSystem.js
+++ database/Recommendations/recommendationSystem.js
@@ -1,3 +1,4 @@
+
 const axios = require('axios');
 const { 
   topicScore, 
@@ -7,21 +8,47 @@ const {
   avgConvoTime
 } = require('./filterByCategories');
 
-axios.get('/recommendation')
-.then((data) => {
-  let currentUser = data.currentUser;
-  let allMentors = data.allMentors;
+const {
+  userWordCounts,
+  wordCountScore
+} = require('./wordCount');
 
-  // Get only the mentors that mentor in topics the 
-  // current user is interested in
-  let filteredByTopic = scoreByTopic(currentUser, allMentors);
-  // Score the mentors based on if they are within a certain
-  // age range of the current user
-  scoreByAge(filteredByTopic);
-  // Score the mentors based on if they are active within
-  // the same avg time as current user
-  avgActiveTime(filteredByTopic);
-  // Score the mentors based on if they have around the 
-  // same avg convo time as the current user
-  avgConvoTime(filteredByTopic);
-});
+const recommendationSystem = (callback) => {
+  let filteredByTopic;
+
+  axios.get('/recommendation')
+  .then((res) => {
+    let currentUser = res.data.currentUser;
+    let age = currentUser.age || null;
+    let allMentors = res.data.allMentors;
+    let userCategories = res.data.userCategories;
+
+    // Get only the mentors that mentor in topics the 
+    // current user is interested in
+    filteredByTopic = scoreByTopic(userCategories, allMentors);
+    // Score the mentors based on if they are within a certain
+    // age range of the current user
+    if (age) {
+      scoreByAge(currentUser, filteredByTopic);
+    }
+    // Score the mentors based on if they are active within
+    // the same avg time as current user
+    avgActiveTime(currentUser, filteredByTopic);
+    // Score the mentors based on if they have around the 
+    // same avg convo time as the current user
+    avgConvoTime(currentUser, filteredByTopic);
+    // Create a score based on matching text input between
+    // the current user and mentors
+    wordCountScore(currentUser, filteredByTopic);
+
+    filteredByTopic.sort((a, b) => {
+      return b.score - a.score;
+    });
+  }).then(() => {
+    callback(filteredByTopic);
+  });
+};
+
+module.exports = {
+  recommendationSystem
+};
