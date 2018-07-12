cf4b5f45c4e99a15fee3b7c454364ff8fc3d3162
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-11 18:51:18 -0700

    Added Card and ListItem styling to component

--- client/components/RecommendedMentors.jsx
+++ client/components/RecommendedMentors.jsx
@@ -1,32 +1,61 @@
-import React, { Component } from 'react';
 import axios from 'axios';
-import { recommendationSystem } from '../../database/Recommendations/recommendationSystem';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import ListItem from '@material-ui/core/ListItem';
+import CardContent from '@material-ui/core/CardContent';
 
-export default class RecommendedMentors extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recommended: []
-    }
-  }
 
-  componentDidMount() {
-    axios.get('/generateMessages')
-
-    recommendationSystem((recommendedMentors) => {
-      this.setState({
-        recommended: recommendedMentors
-      });
-
-      console.log('This is recommendedMentors', recommendedMentors)
-    });
-  }
-  
-  render() {
-    return(
-      <div>
-        This is recommendations
-      </div>
-    );
+const styles = {
+  card: {
+    
   }
 };
+
+class RecommendedMentors extends Component {
+  constructor(props) {
+    super(props);
+
+    this.capitalize = this.capitalize.bind(this);
+  }
+
+  capitalize (words) {
+    words = words.split(' ');
+    let capitalized = [];
+
+    words.forEach((word) => {
+      capitalized.push(`${word[0].toUpperCase()}${word.slice(1)}`);
+    })
+
+    return capitalized.join(' ');
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return(
+      <div>
+        <Card>
+          <CardContent>
+            {this.props.recommended.map((mentor, idx) => {
+              return (
+                <ListItem key={idx} className='recommended'>
+                  <Avatar src={mentor.photo} />
+                  <h2>{ `${this.capitalize(mentor.fullName)}` }</h2>
+                </ListItem>
+              );
+            })}
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+};
+
+RecommendedMentors.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecommendedMentors);
