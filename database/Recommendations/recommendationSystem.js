f662ec09b58d3d3db46dcd20b5918ed810e8b444
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-11 18:54:57 -0700

    Changed var to a more fitting name

--- database/Recommendations/recommendationSystem.js
+++ database/Recommendations/recommendationSystem.js
@@ -13,7 +13,7 @@ const {
 } = require('./wordCount');
 
 const recommendationSystem = (callback) => {
-  let filteredByTopic;
+  let filtered;
 
   axios.get('/recommendation')
   .then((res) => {
@@ -22,29 +22,35 @@ const recommendationSystem = (callback) => {
     let allMentors = res.data.allMentors;
     let userCategories = res.data.userCategories;
 
+    console.log('This is all mentors', allMentors)
+
     // Get only the mentors that mentor in topics the 
     // current user is interested in
-    filteredByTopic = scoreByTopic(userCategories, allMentors);
+    filtered = scoreByTopic(userCategories, allMentors);
     // Score the mentors based on if they are within a certain
     // age range of the current user
     if (age) {
-      scoreByAge(currentUser, filteredByTopic);
+      scoreByAge(currentUser, filtered);
     }
     // Score the mentors based on if they are active within
     // the same avg time as current user
-    avgActiveTime(currentUser, filteredByTopic);
+    avgActiveTime(currentUser, filtered);
     // Score the mentors based on if they have around the 
     // same avg convo time as the current user
-    avgConvoTime(currentUser, filteredByTopic);
+    avgConvoTime(currentUser, filtered);
     // Create a score based on matching text input between
     // the current user and mentors
-    wordCountScore(currentUser, filteredByTopic);
+    wordCountScore(currentUser, filtered);
 
-    filteredByTopic.sort((a, b) => {
+    console.log('This is filtered', filtered)
+
+    filtered.sort((a, b) => {
       return b.score - a.score;
     });
+
+    filtered = (filtered.length > 5) ? filtered.slice(0, 5) : filtered;
   }).then(() => {
-    callback(filteredByTopic);
+    callback(filtered);
   });
 };
 
