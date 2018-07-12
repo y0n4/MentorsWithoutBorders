7f9e3df3af98cd0b5f10e0e107016a0b694730fa
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-07-11 18:48:45 -0700

    Ternary operator to conditional render recommendedMentor component if array.length is greater than 0

--- client/components/MenteeHome/SearchBar.jsx
+++ client/components/MenteeHome/SearchBar.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import RecommendedMentors from '../RecommendedMentors';
+import { recommendationSystem } from '../../../database/Recommendations/recommendationSystem';
 
 export default class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { input: '' };
+    this.state = { 
+      input: '',
+      recommended: []
+    };
 
     this.onInputChange = this.onInputChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    // axios.get('/generateMessages')
+
+    recommendationSystem((recommendedMentors) => {
+      this.setState({
+        recommended: recommendedMentors
+      });
+    });
   }
 
   onInputChange(event) {
@@ -22,20 +37,29 @@ export default class SearchBar extends Component {
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit} className="searchContainer">
-        <input
-          placeholder="search new mentors"
-          className="form-control"
-          value={this.state.input}
-          onChange={this.onInputChange}
-        />
-        <span className="submitButton">
-          <Button type="submit" className="btn btn-secondary">
-Go!
-          </Button>
-        </span>
-      </form>
-
+      <div>
+        <form onSubmit={this.onFormSubmit} className="searchContainer">
+          <input
+            placeholder="search new mentors"
+            className="form-control"
+            value={this.state.input}
+            onChange={this.onInputChange}
+          />
+          <span className="submitButton">
+            <Button type="submit" className="btn btn-secondary">
+  Go!
+            </Button>
+          </span>
+        </form>
+        {(this.state.recommended.length === 0) ? null : 
+          (
+          <div>
+            <h1>Recommended for You</h1>
+            <RecommendedMentors recommended={this.state.recommended} />
+          </div>
+          )
+        }
+      </div>  
     );
   }
 }
