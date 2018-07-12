8bc0c03301f66f2a401d555b519321daf9ae5c5f
yona <04l.yona@gmail.com>
2018-07-12 19:05:20 -0400

    Remove extra lines and typo fix

--- client/components/MentorSignUp.jsx
+++ client/components/MentorSignUp.jsx
@@ -27,25 +27,28 @@ class MentorSignUp extends Component {
         <div className="mentor-descrip">
           <h2>So you want to be in our mentoring community?</h2>
 
-          Anyone can be a mentor in our community. It feels so rewarding when helping a fellow mentee grow, gain knowledge, and be thankful for your efforts! Not only are you giving, you are also learning to be patient, understanding, and more about yourself along the way. <br />
+          <p className="mentor-snippet">
+          Anyone can be a mentor in our community. It feels so rewarding when helping a fellow mentee grow, gain knowledge, and be thankful for your efforts! Not only are you giving, you are also learning to be patient, understanding, and more about yourself along the way.
+          </p>
           <br />
-  
           Simply click on the button below!<br /><br />
           <button className="login-btn" onClick={this.isMentorNow}>
             Sign me up!
-          </button>
+          </button><br /><br />
         </div>
       );
     } else {
       return (
         <div className="mentor-descrip">
           <h2>Congrats you're a mentor!</h2>
-          Thanks for singing up! You now have access to Mentor Mode and see questions posted by mentees around the world! You can view these questions in all categories or filtering them out with specific ones instead. Just chat with them to answer their question!<br /><br />
+          <p className="mentor-snippet">
+          Thanks for signing up! You now have access to Mentor Mode and see questions posted by mentees around the world! You can view these questions in all categories or filtering them out with specific ones instead. Just chat with them to answer their question!
+          </p><br /><br />
           <button className="login-btn">
           <Link to="/mentor">
             Let's go!
           </Link>
-          </button>
+          </button><br /><br />
         </div>
       );
     }
