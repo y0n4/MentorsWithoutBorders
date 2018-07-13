42a984c0adea42310d045aab91089f2de465f595
yona <04l.yona@gmail.com>
2018-07-13 04:33:31 -0400

    Write funcs to render the quotes to page

--- client/components/MenteeHome/MenteeHome.jsx
+++ client/components/MenteeHome/MenteeHome.jsx
@@ -6,6 +6,8 @@ import Grid from '@material-ui/core/Grid';
 import { Redirect, Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
 import MembersOnline from './MembersOnline';
+import MenteeFeed from './MenteeFeed';
+
 
 const styles = theme => ({
   root: {
@@ -19,10 +21,7 @@ const styles = theme => ({
     padding: theme.spacing.unit * 2,
     textAlign: 'center',
     color: theme.palette.text.secondary,
-    // backgroundColor: 'transparent',
     fontFamily: 'sans-serif',
-    // fontWeight: 'bold',
-    // boxShadow: 'none',
     overflow: 'auto',
   },
 });
@@ -32,11 +31,26 @@ class MenteeHome extends Component {
     super(props);
     this.state = {
       userId: this.props.userId,
+      quotes: [],
       isMentor: this.props.isMentor,
       question: '',
     };
     this.onChanges = this.onChange.bind(this);
     this.saveMenteeQ = this.saveMenteeQ.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('getting mounted');
+    // get all questions
+    axios.get('/seeInput', {
+      params: {
+        type: 'quote',
+        userId: this.state.userId,
+      }
+    }).then((res) => {
+      this.setState({ quotes: res.data });
+      console.log(this.state.quotes)
+    });
   }
 
   checkTime() {
@@ -66,60 +80,43 @@ class MenteeHome extends Component {
     });
   }
 
+  renderMentorQs() {
+    return (
+      <div>
+        {this.state.quotes.reverse().map(info => <MenteeFeed info={info} />)}
+      </div>
+    );
+  }
+
+
   render() {
     const { classes, userId } = this.props;
 
     return (
       <div className={classes.root}>
-          <div className="checkTime">
-            {this.checkTime()}<br />
-          </div>
-          <Grid container spacing={24}>
-            <Grid item xs={8} style={{ height: 400, overflow: 'auto' }}>
-              <Paper className={classes.paper}>
-              <div className="input-descrip">
-                Ask a question that mentors can help answer!<br /><br />
-                <textarea 
-                className="input-value"
-                value={this.state.question}
-                onChange={this.onChange} /><br />
-                <button onClick={this.saveMenteeQ}>Submit</button><br /><br /><br />
-              </div>
-                <div className="mentor-quote-entry">
-                Somone is sitting in the shade today because someone planted a tree a long time ago
-                  <bold style={{ color: 'blue' }}>
-                    {' -  Warren Buffet'}
-                  </bold>
-                </div>
-                <div className="mentor-quote-entry">
-                Somone is sitting in the shade today because someone planted a tree a long time ago
-                  <bold style={{ color: 'blue' }}>
-                    {' -  Warren Buffet'}
-                  </bold>
-                </div>
-                {' '}
-                <div className="mentor-quote-entry">
-                Somone is sitting in the shade today because someone planted a tree a long time ago
-                  <bold style={{ color: 'blue' }}>
-                    {' -  Warren Buffet'}
-                  </bold>
-                </div>
-                {' '}
-                <div className="mentor-quote-entry">
-                Somone is sitting in the shade today because someone planted a tree a long time ago
-                  <bold style={{ color: 'blue' }}>
-                    {' -  Warren Buffet'}
-                  </bold>
-                </div>
-                {' '}
-                <div className="mentor-quote-entry">
-                Somone is sitting in the shade today because someone planted a tree a long time ago
-                  <bold style={{ color: 'blue' }}>
-                    {' -  Warren Buffet'}
-                  </bold>
-                </div>
-              </Paper>
-            </Grid>
+        <div className="checkTime">
+          {this.checkTime()}<br />
+        </div>
+        <Grid container spacing={24}>
+          <Grid item xs={8} style={{ height:650, overflow: 'auto' }}>
+            <Paper className={classes.paper}>
+            <div className="input-descrip">
+              Ask a question that mentors can help answer!<br /><br />
+              <textarea 
+              className="input-value"
+              value={this.state.question}
+              onChange={this.onChange} /><br />
+              <button onClick={this.saveMenteeQ}>Submit</button><br /><br /><br />
+            </div>
+            <div className="mentee-question-feed">
+              Help guide your mentee's with tips that can help answer their worries/questions!
+              <br />
+                Simply visit their profile and chat with them!
+              <br />
+              {this.renderMentorQs()}
+            </div>
+            </Paper>
+          </Grid>
             <Grid item xs={4} style={{ height: 500 }}>
               <Paper className={classes.paper}>
                 <div>
